70aed68b34c25bfeef1e57746f56028e95dae639
David Füchslin <david.fuchslin@gmail.com>
2020-08-17 18:58:04 +0200

    classify

---
+++ lib/reporter.py
@@ -0,0 +1,143 @@
+import pycurl
+from urllib.parse import urlparse
+import sys, getopt, os
+import socket
+import time
+try:
+    from io import BytesIO
+except ImportError:
+    from StringIO import StringIO as BytesIO
+
+class CurlTimeReporter:
+
+    def __init__(self, config):
+        self.config = config
+
+    def calc_diff(self, current, running_response_time):
+        if current - running_response_time < 0.0:
+            return 0
+        return current - running_response_time
+
+    def get_url_stats(self):
+        url = self.config['url']
+        timestamp = int(time.time())
+        buffer = BytesIO()
+        c = pycurl.Curl()
+        c.setopt(pycurl.USERAGENT, self.config['user_agent'])
+        c.setopt(c.TIMEOUT, self.config['timeout'])
+        c.setopt(c.URL, url)
+        c.setopt(c.WRITEDATA, buffer)
+        if len(self.config['headers']) > 0:
+          c.setopt(c.HTTPHEADER, [ k+': '+v for k,v in self.config['headers'].items() ])
+
+        stats = {}
+        stats["status.success"] = 1
+        stats["status.timedout"] = 0
+        stats["status.error"] = 0
+
+        try:
+            c.perform()
+        except Exception as e:
+            if e.args[0] == c.E_OPERATION_TIMEDOUT:
+                stats["status.timedout"] = 1
+                stats["status.success"] = 0
+            else:
+                print(e)
+                stats["status.error"] = 1
+                stats["status.success"] = 0
+
+        # deprecate these
+        stats["namelookuptime"] = c.getinfo(c.NAMELOOKUP_TIME)
+        stats["connecttime"] = c.getinfo(c.CONNECT_TIME)
+        stats["appconnecttime"] = c.getinfo(c.APPCONNECT_TIME)
+        stats["pretransfertime"] = c.getinfo(c.PRETRANSFER_TIME)
+        stats["starttransfertime"] = c.getinfo(c.STARTTRANSFER_TIME)
+        stats["responsetime"] = c.getinfo(c.TOTAL_TIME)
+        stats["redirecttime"] = c.getinfo(c.REDIRECT_TIME)
+
+        # calculate diffs
+        running_response_time = 0.0
+
+        stats["time.namelookup.reported"] = c.getinfo(c.NAMELOOKUP_TIME)
+        stats["time.namelookup.diff"] = self.calc_diff(c.getinfo(c.NAMELOOKUP_TIME), running_response_time)
+        running_response_time += stats["time.namelookup.diff"]
+
+        stats["time.connect.reported"] = c.getinfo(c.CONNECT_TIME)
+        stats["time.connect.diff"] = self.calc_diff(c.getinfo(c.CONNECT_TIME), running_response_time)
+        running_response_time += stats["time.connect.diff"]
+
+        stats["time.appconnect.reported"] = c.getinfo(c.APPCONNECT_TIME)
+        stats["time.appconnect.diff"] = self.calc_diff(c.getinfo(c.APPCONNECT_TIME), running_response_time)
+        running_response_time += stats["time.appconnect.diff"]
+
+        stats["time.pretransfer.reported"] = c.getinfo(c.PRETRANSFER_TIME)
+        stats["time.pretransfer.diff"] = self.calc_diff(c.getinfo(c.PRETRANSFER_TIME), running_response_time)
+        running_response_time += stats["time.pretransfer.diff"]
+
+        stats["time.starttransfer.reported"] = c.getinfo(c.STARTTRANSFER_TIME)
+        stats["time.starttransfer.diff"] = self.calc_diff(c.getinfo(c.STARTTRANSFER_TIME), running_response_time)
+        running_response_time += stats["time.starttransfer.diff"]
+
+        stats["time.redirect.reported"] = c.getinfo(c.REDIRECT_TIME)
+        stats["time.redirect.diff"] = self.calc_diff(c.getinfo(c.REDIRECT_TIME), running_response_time)
+        running_response_time += stats["time.redirect.diff"]
+
+        stats["time.total.reported"] = c.getinfo(c.TOTAL_TIME)
+        stats["time.total.diff"] = self.calc_diff(c.getinfo(c.TOTAL_TIME), running_response_time)
+        running_response_time += stats["time.total.diff"]
+
+        stats["responsecode"] = c.getinfo(c.RESPONSE_CODE)
+        stats["downloadbytes"] = c.getinfo(c.SIZE_DOWNLOAD)
+        stats["timestamp"] = timestamp
+        stats["url"] = url
+
+        c.close()
+
+        return stats
+
+    def build_message(self, stat, datatype, graphite_path, stats):
+        return (("{}.{} {:" + datatype + "} {:d}\n").format(graphite_path, stat, stats[stat], stats["timestamp"]))
+
+    def build_graphite_friendly_url(self, url):
+        return url.replace("https://", "").replace("http://", "").replace("/", "_").replace("?", "_").replace("&", "_").replace(".", "_")
+
+    def send_single_status(self, msg):
+        print('sending message to graphite: %s' % msg.replace('\n', ''), flush=True)
+        sock = socket.socket()
+        sock.connect((self.config['graphite_host'], self.config['graphite_port']))
+        sock.sendall(msg.encode())
+        sock.close()
+
+    def send_stats(self, stats):
+        graphite_path = "{}.{}".format(self.config['graphite_metric_prefix'], self.build_graphite_friendly_url(stats["url"]))
+
+        # deprecate these
+        self.send_single_status(self.build_message("namelookuptime", "f", graphite_path, stats))
+        self.send_single_status(self.build_message("connecttime", "f", graphite_path, stats))
+        self.send_single_status(self.build_message("appconnecttime", "f", graphite_path, stats))
+        self.send_single_status(self.build_message("pretransfertime", "f", graphite_path, stats))
+        self.send_single_status(self.build_message("starttransfertime", "f", graphite_path, stats))
+        self.send_single_status(self.build_message("redirecttime", "f", graphite_path, stats))
+        self.send_single_status(self.build_message("responsetime", "f", graphite_path, stats))
+
+        self.send_single_status(self.build_message("time.namelookup.reported", "f", graphite_path, stats))
+        self.send_single_status(self.build_message("time.namelookup.diff", "f", graphite_path, stats))
+        self.send_single_status(self.build_message("time.connect.reported", "f", graphite_path, stats))
+        self.send_single_status(self.build_message("time.connect.diff", "f", graphite_path, stats))
+        self.send_single_status(self.build_message("time.appconnect.reported", "f", graphite_path, stats))
+        self.send_single_status(self.build_message("time.appconnect.diff", "f", graphite_path, stats))
+        self.send_single_status(self.build_message("time.pretransfer.reported", "f", graphite_path, stats))
+        self.send_single_status(self.build_message("time.pretransfer.diff", "f", graphite_path, stats))
+        self.send_single_status(self.build_message("time.starttransfer.reported", "f", graphite_path, stats))
+        self.send_single_status(self.build_message("time.starttransfer.diff", "f", graphite_path, stats))
+        self.send_single_status(self.build_message("time.redirect.reported", "f", graphite_path, stats))
+        self.send_single_status(self.build_message("time.redirect.diff", "f", graphite_path, stats))
+        self.send_single_status(self.build_message("time.total.reported", "f", graphite_path, stats))
+        self.send_single_status(self.build_message("time.total.diff", "f", graphite_path, stats))
+
+        self.send_single_status(self.build_message("status.success", "d", graphite_path, stats))
+        self.send_single_status(self.build_message("status.timedout", "d", graphite_path, stats))
+        self.send_single_status(self.build_message("status.error", "d", graphite_path, stats))
+
+        self.send_single_status(self.build_message("responsecode", "d", graphite_path, stats))
+        self.send_single_status(self.build_message("downloadbytes", "f", graphite_path, stats))
--- uptime.py
+++ uptime.py
@@ -1,148 +1,13 @@
 #! /usr/bin/env python3
 
-import pycurl
-from urllib.parse import urlparse
-import sys, getopt, os
-import socket
+import sys, os
 import time
-try:
-    from io import BytesIO
-except ImportError:
-    from StringIO import StringIO as BytesIO
+from lib.reporter import CurlTimeReporter
 
 def get_env_var(name, default):
     if name in os.environ:
         return os.environ[name]
     return default
-
-def calc_diff(current, running_response_time):
-    if current - running_response_time < 0.0:
-        return 0
-    return current - running_response_time
-
-def get_url_stats(config):
-    url = config['url']
-    timestamp = int(time.time())
-    buffer = BytesIO()
-    c = pycurl.Curl()
-    c.setopt(pycurl.USERAGENT, config['user_agent'])
-    c.setopt(c.TIMEOUT, config['timeout'])
-    c.setopt(c.URL, url)
-    c.setopt(c.WRITEDATA, buffer)
-    if len(config['headers']) > 0:
-      c.setopt(c.HTTPHEADER, [ k+': '+v for k,v in config['headers'].items() ])
-
-    stats = {}
-    stats["status.success"] = 1
-    stats["status.timedout"] = 0
-    stats["status.error"] = 0
-
-    try:
-        c.perform()
-    except Exception as e:
-        if e.args[0] == c.E_OPERATION_TIMEDOUT:
-            stats["status.timedout"] = 1
-            stats["status.success"] = 0
-        else:
-            print(e)
-            stats["status.error"] = 1
-            stats["status.success"] = 0
-
-    # deprecate these
-    stats["namelookuptime"] = c.getinfo(c.NAMELOOKUP_TIME)
-    stats["connecttime"] = c.getinfo(c.CONNECT_TIME)
-    stats["appconnecttime"] = c.getinfo(c.APPCONNECT_TIME)
-    stats["pretransfertime"] = c.getinfo(c.PRETRANSFER_TIME)
-    stats["starttransfertime"] = c.getinfo(c.STARTTRANSFER_TIME)
-    stats["responsetime"] = c.getinfo(c.TOTAL_TIME)
-    stats["redirecttime"] = c.getinfo(c.REDIRECT_TIME)
-
-    # calculate diffs
-    running_response_time = 0.0
-
-    stats["time.namelookup.reported"] = c.getinfo(c.NAMELOOKUP_TIME)
-    stats["time.namelookup.diff"] = calc_diff(c.getinfo(c.NAMELOOKUP_TIME), running_response_time)
-    running_response_time += stats["time.namelookup.diff"]
-
-    stats["time.connect.reported"] = c.getinfo(c.CONNECT_TIME)
-    stats["time.connect.diff"] = calc_diff(c.getinfo(c.CONNECT_TIME), running_response_time)
-    running_response_time += stats["time.connect.diff"]
-
-    stats["time.appconnect.reported"] = c.getinfo(c.APPCONNECT_TIME)
-    stats["time.appconnect.diff"] = calc_diff(c.getinfo(c.APPCONNECT_TIME), running_response_time)
-    running_response_time += stats["time.appconnect.diff"]
-
-    stats["time.pretransfer.reported"] = c.getinfo(c.PRETRANSFER_TIME)
-    stats["time.pretransfer.diff"] = calc_diff(c.getinfo(c.PRETRANSFER_TIME), running_response_time)
-    running_response_time += stats["time.pretransfer.diff"]
-
-    stats["time.starttransfer.reported"] = c.getinfo(c.STARTTRANSFER_TIME)
-    stats["time.starttransfer.diff"] = calc_diff(c.getinfo(c.STARTTRANSFER_TIME), running_response_time)
-    running_response_time += stats["time.starttransfer.diff"]
-
-    stats["time.redirect.reported"] = c.getinfo(c.REDIRECT_TIME)
-    stats["time.redirect.diff"] = calc_diff(c.getinfo(c.REDIRECT_TIME), running_response_time)
-    running_response_time += stats["time.redirect.diff"]
-
-    stats["time.total.reported"] = c.getinfo(c.TOTAL_TIME)
-    stats["time.total.diff"] = calc_diff(c.getinfo(c.TOTAL_TIME), running_response_time)
-    running_response_time += stats["time.total.diff"]
-
-    stats["responsecode"] = c.getinfo(c.RESPONSE_CODE)
-    stats["downloadbytes"] = c.getinfo(c.SIZE_DOWNLOAD)
-    stats["timestamp"] = timestamp
-    stats["url"] = url
-
-    c.close()
-
-    return stats
-
-def build_message(stat, datatype, graphite_path, stats):
-    return (("{}.{} {:" + datatype + "} {:d}\n").format(graphite_path, stat, stats[stat], stats["timestamp"]))
-
-def build_graphite_friendly_url(url):
-  return url.replace("https://", "").replace("http://", "").replace("/", "_").replace("?", "_").replace("&", "_").replace(".", "_")
-
-def send_stats(stats, config):
-    graphite_path = "{}.{}".format(config['graphite_metric_prefix'], build_graphite_friendly_url(stats["url"]))
-
-    # deprecate these
-    send_single_status(build_message("namelookuptime", "f", graphite_path, stats), config)
-    send_single_status(build_message("connecttime", "f", graphite_path, stats), config)
-    send_single_status(build_message("appconnecttime", "f", graphite_path, stats), config)
-    send_single_status(build_message("pretransfertime", "f", graphite_path, stats), config)
-    send_single_status(build_message("starttransfertime", "f", graphite_path, stats), config)
-    send_single_status(build_message("redirecttime", "f", graphite_path, stats), config)
-    send_single_status(build_message("responsetime", "f", graphite_path, stats), config)
-
-    send_single_status(build_message("time.namelookup.reported", "f", graphite_path, stats), config)
-    send_single_status(build_message("time.namelookup.diff", "f", graphite_path, stats), config)
-    send_single_status(build_message("time.connect.reported", "f", graphite_path, stats), config)
-    send_single_status(build_message("time.connect.diff", "f", graphite_path, stats), config)
-    send_single_status(build_message("time.appconnect.reported", "f", graphite_path, stats), config)
-    send_single_status(build_message("time.appconnect.diff", "f", graphite_path, stats), config)
-    send_single_status(build_message("time.pretransfer.reported", "f", graphite_path, stats), config)
-    send_single_status(build_message("time.pretransfer.diff", "f", graphite_path, stats), config)
-    send_single_status(build_message("time.starttransfer.reported", "f", graphite_path, stats), config)
-    send_single_status(build_message("time.starttransfer.diff", "f", graphite_path, stats), config)
-    send_single_status(build_message("time.redirect.reported", "f", graphite_path, stats), config)
-    send_single_status(build_message("time.redirect.diff", "f", graphite_path, stats), config)
-    send_single_status(build_message("time.total.reported", "f", graphite_path, stats), config)
-    send_single_status(build_message("time.total.diff", "f", graphite_path, stats), config)
-
-    send_single_status(build_message("status.success", "d", graphite_path, stats), config)
-    send_single_status(build_message("status.timedout", "d", graphite_path, stats), config)
-    send_single_status(build_message("status.error", "d", graphite_path, stats), config)
-
-    send_single_status(build_message("responsecode", "d", graphite_path, stats), config)
-    send_single_status(build_message("downloadbytes", "f", graphite_path, stats), config)
-
-def send_single_status(msg, config):
-    print('sending message to graphite: %s' % msg.replace('\n', ''), flush=True)
-    sock = socket.socket()
-    sock.connect((config['graphite_host'], config['graphite_port']))
-    sock.sendall(msg.encode())
-    sock.close()
 
 def main(argv):
     if len(argv) < 2:
@@ -162,8 +27,10 @@ def main(argv):
     if get_env_var("USE_GZIP", "true").lower() == "true":
         config['headers'] = { "accept-encoding": "gzip, deflate" }
 
+    reporter = CurlTimeReporter(config)
+
     while(True):
-        send_stats(get_url_stats(config), config)
+        reporter.send_stats(reporter.get_url_stats())
         time.sleep(config['check_interval'])
 
 main(sys.argv)
